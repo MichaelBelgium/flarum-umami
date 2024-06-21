238317ad08b99962e87163a6c1bc072d31b395e1
Michael V <michaelvrld@gmail.com>
2024-06-21 11:38:51 +0200

    Add settings

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,5 +1,20 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('michaelbelgium/flarum-umami', () => {
-  console.log('[michaelbelgium/flarum-umami] Hello, admin!');
+  app.extensionData
+    .for('michaelbelgium-umami')
+    .registerSetting({
+      setting: 'michaelbelgium-umami.site_id',
+      label: app.translator.trans('michaelbelgium-umami.admin.settings.site_id.label'),
+      help: app.translator.trans('michaelbelgium-umami.admin.settings.site_id.help'),
+      placeholder: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx',
+      type: 'text',
+    })
+    .registerSetting({
+      setting: 'michaelbelgium-umami.domain',
+      label: app.translator.trans('michaelbelgium-umami.admin.settings.domain.label'),
+      help: app.translator.trans('michaelbelgium-umami.admin.settings.domain.help'),
+      placeholder: 'https://umami.yourdomain.com',
+      type: 'text',
+    });
 });
